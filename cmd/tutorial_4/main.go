b697727124cf57c3646423997341fdbbf54a28b4
fcvirtual <austin@fcvt.dev>
2024-04-29 17:00:11 -0400

    Arrays Slices Maps and Loops

---
+++ cmd/tutorial_4/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	intArr := [...]int32{1, 2, 3}
+	fmt.Println(intArr)
+
+	var intSlice []int32 = []int32{4, 5, 6}
+	fmt.Println(intSlice)
+	intSlice = append(intSlice, 7)
+	fmt.Println(intSlice)
+}
