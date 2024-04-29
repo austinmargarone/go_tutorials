61c6c79e6406e0496043b7310166c493a9a39556
fcvirtual <austin@fcvt.dev>
2024-04-29 17:02:42 -0400

    Arrays Slices Maps and Loops

--- cmd/tutorial_4/main.go
+++ cmd/tutorial_4/main.go
@@ -10,4 +10,17 @@ func main() {
 	fmt.Println(intSlice)
 	intSlice = append(intSlice, 7)
 	fmt.Println(intSlice)
+
+	var intSlice2 []int32 = []int32{8, 9}
+	intSlice = append(intSlice, intSlice2...)
+	fmt.Println(intSlice)
+
+	var intSlice3 []int32 = make([]int32, 3, 10)
+	fmt.Println(intSlice3)
+
+	var myMap map[string]int32 = map[string]int32{"one": 1, "two": 2}
+	fmt.Println(myMap)
+
+	var myMap2 map[string]int32 = make(map[string]int32)
+	myMap2["three"] = 3
 }
